c65aeb203164ca56e2999dbd2c29ecbc2e3752d7
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-09-22 23:42:48 +0700

    Python Day 3 operators, variable

---
+++ intro.py
@@ -0,0 +1,23 @@
+# print("San Kyi Tar")
+# print(20+5)
+# print(20-5)
+# print(20*5)
+# print(20/5)
+# print(20%5)
+# print(20**5)
+# print(20//5)
+
+# x = 5
+# y = "San Kyi tar"
+# firstname = "San Kyi"
+# lastName = "Tar"
+
+# myName = "ha ha"
+ages = [20,39,3]
+print(ages)
+
+# print(firstName + " " + lastName)
+
+
+# print(x)
+# print(y)
